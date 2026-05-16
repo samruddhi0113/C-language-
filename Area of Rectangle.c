d6421b48a7150497ee00b3b3029f3f68f26b7fc0
samruddhi0113 <samruddhi13jan@gmail.com>
2026-05-16 21:49:46 +0530

    Add files via upload

---
+++ Area of Rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	float L,W,Rectangle;
+	printf("Enter Length :");
+	scanf("%f",&L);
+	printf("Enter Width :");
+	scanf("%f",&W);
+	Rectangle=L*W;
+	printf("Area of Rectangle %.2f",Rectangle);
+}
